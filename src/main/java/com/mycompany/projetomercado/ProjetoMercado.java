eab920ec9ba6948a11cf6263bf154121a32b4600
Giuliano Nolasco <110136470+GiulianoNolasco@users.noreply.github.com>
2022-08-05 20:44:04 -0300

    Commit inicial

---
+++ src/main/java/com/mycompany/projetomercado/ProjetoMercado.java
@@ -0,0 +1,35 @@
+package com.mycompany.projetomercado;
+
+import VIEW.TelaPrincipal;
+
+public class ProjetoMercado {
+
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+
+    }
+}
+
+// ClientesDTO CliDTO = new ClientesDTO();
+//        ComandosDAO DAO = new ComandosDAO();
+//        CliDTO.setIdade_cliente(23);
+//        CliDTO.setNome_cliente("lUCAS");
+//        CliDTO.setSituacao_cliente('A');
+//        String comando = DAO.insereRegistro(CliDTO);
+//        ClientesDTO CliDTO = new ClientesDTO();
+//        ComandosDAO DAO = new ComandosDAO();
+//        CliDTO.setId_cliente(1);
+//        String comando = DAO.apagaRegistro(CliDTO);
+//        System.out.println(comando);
+//            ClientesDTO CliDto = new ClientesDTO();
+//            ComandosDAO DAO = new ComandosDAO();
+//            CliDto.setId_cliente(2);
+//            CliDto.setIdade_cliente(222);
+//            CliDto.setNome_cliente("TESTE ALTERACAO");
+//            CliDto.setSituacao_cliente('I');
+//            String comando = DAO.atualizaRegistro(CliDto);
+//            System.out.println(comando);
+        
+//        ComandosDAO DAO = new ComandosDAO();
+//        DAO.retornaRegistro(ClientesDTO);
